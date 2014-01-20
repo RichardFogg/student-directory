faf8fa44142dfa64a5362ae8c9cc35402045fa49
Richard Fogg <rpfogg@gmail.com>
2014-01-20 21:32:20 +0000

    Comments added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "Abraham Thomas"
 puts "Andy Parton"
@@ -15,6 +16,8 @@ puts "Peter Hitchcock"
 puts "Samantha Harris"
 puts "Yvette Cook"
 puts "David Thompson"
+# finally, we print the total
 print "Overall, we have "
+# it's important that print () doesnt add new line characters
 print 16
 puts " great students"

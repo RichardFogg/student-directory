fbcb4bbf2a3185563db6c61a7c19cc77f8234bde
Richard Fogg <rpfogg@gmail.com>
2014-01-29 14:54:42 +0000

    User Input added

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
-#student_count = 16
+student_count = 16
 # let's put all students into an array
-#students = [
+students = [
 
-#{:name => "Abraham Thomas", :cohort=> :January},
-#{:name => "Andy Parton" , :cohort=> :January},
-#{:name => "Damien Hogan" , :cohort=> :January},
-#{:name => "Dean Bryen" , :cohort=> :January},
-#{:name => "Dharmesh Raithatha" , :cohort=> :January},
-#{:name => "Enrique Comba Riepenhausen" , :cohort=> :January},
-#{:name => "Joan Senent" , :cohort=> :January},
-#{:name => "Katrina Harradine" , :cohort=> :January},
-#{:name => "John Damico" , :cohort=> :January},
-#{:name => "Jorja Tracy Hung" , :cohort=> :January},
-#{:name => "Kiy-Hong Tang" , :cohort=> :January},
-#{:name => "Peter Saxton" , :cohort=> :January},
-#{:name => "Peter Hitchcock" , :cohort=> :January},
-#{:name => "Samantha Harris" , :cohort=> :January},
-#{:name => "Yvette Cook" , :cohort=> :January},
-#{:name => "David Thompson", :cohort=> :January},
-#]
+{:name => "Abraham Thomas", :cohort=> :January},
+{:name => "Andy Parton" , :cohort=> :January},
+{:name => "Damien Hogan" , :cohort=> :January},
+{:name => "Dean Bryen" , :cohort=> :January},
+{:name => "Dharmesh Raithatha" , :cohort=> :January},
+{:name => "Enrique Comba Riepenhausen" , :cohort=> :January},
+{:name => "Joan Senent" , :cohort=> :January},
+{:name => "Katrina Harradine" , :cohort=> :January},
+{:name => "John Damico" , :cohort=> :January},
+{:name => "Jorja Tracy Hung" , :cohort=> :January},
+{:name => "Kiy-Hong Tang" , :cohort=> :January},
+{:name => "Peter Saxton" , :cohort=> :January},
+{:name => "Peter Hitchcock" , :cohort=> :January},
+{:name => "Samantha Harris" , :cohort=> :January},
+{:name => "Yvette Cook" , :cohort=> :January},
+{:name => "David Thompson", :cohort=> :January},
+]
 
 def print_header
  #and then print them

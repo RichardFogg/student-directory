fc0fb044d1855453b28ca86044ca350e65e1209b
Richard Fogg <rpfogg@gmail.com>
2014-02-03 21:15:08 +0000

    ready for questions

--- directory.rb
+++ directory.rb
@@ -1,25 +1,3 @@
-student_count = 16
-# let's put all students into an array
-students = [
-
-{:name => "Abraham Thomas", :cohort=> :January},
-{:name => "Andy Parton" , :cohort=> :January},
-{:name => "Damien Hogan" , :cohort=> :January},
-{:name => "Dean Bryen" , :cohort=> :January},
-{:name => "Dharmesh Raithatha" , :cohort=> :January},
-{:name => "Enrique Comba Riepenhausen" , :cohort=> :January},
-{:name => "Joan Senent" , :cohort=> :January},
-{:name => "Katrina Harradine" , :cohort=> :January},
-{:name => "John Damico" , :cohort=> :January},
-{:name => "Jorja Tracy Hung" , :cohort=> :January},
-{:name => "Kiy-Hong Tang" , :cohort=> :January},
-{:name => "Peter Saxton" , :cohort=> :January},
-{:name => "Peter Hitchcock" , :cohort=> :January},
-{:name => "Samantha Harris" , :cohort=> :January},
-{:name => "Yvette Cook" , :cohort=> :January},
-{:name => "David Thompson", :cohort=> :January},
-]
-
 def print_header
  #and then print them
 puts "The students of my cohort at Makers Academy"
@@ -27,13 +5,13 @@ puts "------------------"
 end
 
 def print (students)
-        students.each do |student|
-                puts "#{student[:name]} (#{student[:cohort]} cohort)"
-        end
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort, hobbies #{student[:hobbies]})"
+    end
 end
 
 def print_footer(names)
-        puts "Overall, we have #{names.length} great students"
+    puts "Overall, we have #{names.length} great students"
 end
 
 def input_students
